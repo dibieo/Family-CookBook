3f3ec989c2219c26fe8ec4ab778cd72ef77c7a5b
Ovo Dibie <dibieo@gmail.com>
2011-11-15 10:38:10 -0700

    Integrated the add_recipe page

---
+++ js/home.js
@@ -0,0 +1,19 @@
+function goToAddRecipePage(){
+ window.location = "add-recipe.html";
+}
+
+function viewRecipe() {
+window.location = "view-recipe.html";
+}
+
+function browseRecipe() {
+ 
+window.location = "browse-recipes.html";
+	
+}
+
+function requestRecipe(){
+
+ window.location = "request.html";
+
+}
